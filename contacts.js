cb96bc7a7e3617f0bb9dcc16d00f8e7cdbbc237d
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2022-02-27 19:25:33 +0100

    6/6 Done

--- contacts.js
+++ contacts.js
@@ -1,5 +1,76 @@
 const path = require('path')
 const fs = require('fs').promises
 
+const contactsPath = './db/contacts.json'
 
-const contactsPath = process.cwd()
+const displayContacts = () =>
+  fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contact) => console.table(contact))
+
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) =>
+      contacts.map(({ name, email, phone }) => ({ name, email, phone }))
+    )
+    .finally(displayContacts)
+    .catch((err) => console.log(err.message))
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) =>
+      contacts.find((contact) => Number(contact.id) === Number(contactId))
+    )
+    .finally(displayContacts)
+    .catch((err) => console.log(err.message))
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) => {
+      const isInContacts = () =>
+        contacts.find((contact) => Number(contact.id) === Number(contactId))
+
+      if (!isInContacts()) return
+
+      return fs.writeFile(
+        contactsPath,
+        JSON.stringify(
+          contacts?.filter(
+            (contact) => Number(contact.id) !== Number(contactId)
+          )
+        )
+      )
+    })
+    .finally(displayContacts)
+    .catch((err) => console.log(err.message))
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((contacts) => {
+      const newContact = { id: `${contacts.length + 1}`, name, email, phone }
+
+      const isInContactsPhone = () =>
+        contacts.find(
+          (contact) => Number(contact.phone) === Number(newContact.phone)
+        )
+
+      if (isInContactsPhone()) return
+
+      return fs.writeFile(
+        contactsPath,
+        JSON.stringify([...contacts, newContact])
+      )
+    })
+    .finally(displayContacts)
+    .catch((err) => console.log(err.message))
+}
+
+module.exports = { addContact, removeContact, getContactById, listContacts }
--- index.js
+++ index.js
@@ -1,66 +1,43 @@
-const path = require('path')
-const fs = require('fs').promises
+const {
+  addContact,
+  removeContact,
+  getContactById,
+  listContacts,
+} = require('./contacts')
 
-const contactsPath = './db/contacts.json'
+const { Command } = require('commander')
+const program = new Command()
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone')
 
-function listContacts() {
-  fs.readFile(contactsPath)
-    .then((data) => JSON.parse(data))
-    .then((contacts) =>
-      contacts.map(({ name, email, phone }) => ({ name, email, phone }))
-    )
-    .then((contacts) => console.table(contacts))
-    .catch((err) => console.log(err.message))
+program.parse(process.argv)
+
+const argv = program.opts()
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts()
+      break
+    case 'get':
+      getContactById(id)
+      break
+
+    case 'add':
+      addContact(name, email, phone)
+      break
+
+    case 'remove':
+      removeContact(id)
+      break
+
+    default:
+      console.warn('\x1B[31m Unknown action type!')
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath)
-    .then((data) => JSON.parse(data))
-    .then((contacts) =>
-      contacts.find((contact) => Number(contact.id) === Number(contactId))
-    )
-    .then((contact) => console.table(contact))
-    .catch((err) => console.log(err.message))
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath)
-    .then((data) => JSON.parse(data))
-    .then((contacts) => {
-      const isInContacts = () =>
-        contacts.find((contact) => Number(contact.id) === Number(contactId))
-
-      if (!isInContacts()) return
-
-      return fs.writeFile(
-        contactsPath,
-        JSON.stringify(
-          contacts?.filter(
-            (contact) => Number(contact.id) !== Number(contactId)
-          )
-        )
-      )
-    })
-    .catch((err) => console.log(err.message))
-}
-
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath)
-    .then((data) => JSON.parse(data))
-    .then((contacts) => {
-      const newContact = { id: `${contacts.length + 1}`, name, email, phone }
-
-      const isInContactsPhone = () =>
-        contacts.find(
-          (contact) => Number(contact.phone) === Number(newContact.phone)
-        )
-
-      if (isInContactsPhone()) return
-
-      return fs.writeFile(
-        contactsPath,
-        JSON.stringify([...contacts, newContact])
-      )
-    })
-    .catch((err) => console.log(err.message))
-}
+invokeAction(argv)
